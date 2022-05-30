2465272b0915bca5c8354fbbcfcf4cc05e169d49
sourav <banerjee.sourav54@gmail.com>
2022-05-30 13:21:29 +0530

    Implementation of Book service to get the books from json book database

--- src/main/java/com/sourav/assignment/librarymanagement/repository/BookRepository.java
+++ src/main/java/com/sourav/assignment/librarymanagement/repository/BookRepository.java
@@ -1,12 +1,29 @@
 package com.sourav.assignment.librarymanagement.repository;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.sourav.assignment.librarymanagement.model.Book;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Repository
 public class BookRepository {
+
+    @Value("${bookDatabase}")
+    private String books;
+
+    private List<Book> bookList = null;
+
+    @PostConstruct
+    public void initBooks() {
+        Gson gson = new Gson();
+        bookList = (List<Book>) gson.fromJson(books,
+                new TypeToken<List<Book>>() {
+                }.getType());
+    }
 
     public Book findById(String id) {
         return null;
@@ -14,7 +31,7 @@ public class BookRepository {
 
     public List<Book> findAll() {
 
-        return null;
+        return bookList;
     }
 
 }
